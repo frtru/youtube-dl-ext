a6a9cdc3b0afaff5f4f73da813f32c0e39448946
frtru <francoistrudel13@gmail.com>
2019-07-22 12:46:24 -0400

    Adding the YOUTUBE_DL_EXT_HOST_PATH environment variable that will be used in the code instead of the hardcoded output folder.

--- host/install_host_app.bat
+++ host/install_host_app.bat
@@ -2,3 +2,6 @@
 :: %~dp0 is the directory containing this bat script and ends with a backslash.
 REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\youtubedl" /ve /t REG_SZ /d "%~dp0manifest.json" /f
 REG ADD "HKLM\Software\Google\Chrome\NativeMessagingHosts\youtubedl" /ve /t REG_SZ /d "%~dp0manifest.json" /f
+
+:: MY_PATH must be edited to reflect the correct path on your system
+setx YOUTUBE_DL_EXT_HOST_PATH "MY_PATH"
--- host/uninstall_host_app.bat
+++ host/uninstall_host_app.bat
@@ -1,3 +1,4 @@
 :: Deletes the entry created by install_host_app.bat
 REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\youtubedl" /f
 REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\youtubedl" /f
+REG DELETE "HKCU\Environment" /F /V YOUTUBE_DL_EXT_HOST_PATH
